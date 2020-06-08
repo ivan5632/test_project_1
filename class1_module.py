5efa9e37e049dda87adc0b556255e36eb86a062b
Ivan Budnikov <ivan34543799@gmail.com>
2020-06-08 20:08:51 +0930

    Initial commit

---
+++ class1_module.py
@@ -0,0 +1,8 @@
+class Class1:
+    def __init__(self):
+        self.var1 = 122
+        self.var2 = 2
+
+    def show_var1(self):
+        print(self.var1)
+
---
+++ class2_module.py
@@ -0,0 +1,8 @@
+class Class2:
+    def __init__(self):
+        self.var1 = 999
+        self.var2 = 56
+
+    def show_var1(self):
+        print(self.var1)
+
