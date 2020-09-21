363b6fe4f0bc302ac9bc6e93d2681e2cb93ad5f1
Ivan Budnikov <ivan34543799@gmail.com>
2020-09-21 21:55:16 +0930

    added label to main window

--- QT_Application.py
+++ QT_Application.py
@@ -1,11 +1,19 @@
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
 import sys
+import typing
+
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QLabel, QMainWindow, QWidget
+from PyQt5.QtCore import Qt
 
 
 class MainWindow(QMainWindow):
-    pass
+    def __init__(self, *args, **kwargs):
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setWindowTitle("Application Title")
+        label = QLabel("Label text")
+        label.setAlignment(Qt.AlignLeft)
+
+        self.setCentralWidget(label)
 
 
 app = QApplication(sys.argv)
