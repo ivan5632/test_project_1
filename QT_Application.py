8645753d8ac0d96bdedd6d2d8d95c09ceaac0ab6
Ivan Budnikov <ivan34543799@gmail.com>
2020-09-22 22:04:20 +0930

    played with super()

--- QT_Application.py
+++ QT_Application.py
@@ -11,10 +11,29 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setWindowTitle("Application Title")
         label = QLabel("Label text")
-        label.setAlignment(Qt.AlignLeft)
+        label.setAlignment(Qt.AlignCenter)
 
         self.setCentralWidget(label)
 
+# class Rectangle:
+#     def __init__(self, x_size, y_size):
+#         self.x_size = x_size
+#         self.y_size = y_size
+#
+#     def area(self):
+#         return self.x_size * self.y_size
+
+
+# class Square(Rectangle):
+#     def __init__(self, x_size):
+#         super().__init__(x_size, x_size)
+#
+#
+# r = Rectangle(3, 5)
+# print(r.area())
+# print('------------------------------')
+# s = Square(6)
+# print(s.area())
 
 app = QApplication(sys.argv)
 
