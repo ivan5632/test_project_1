96724031d35fac1e0b26d9421f81d3a5a40d6a2e
Ivan Budnikov <ivan34543799@gmail.com>
2020-09-23 14:52:09 +0930

    played with windowTitleChanged

--- QT_Application.py
+++ QT_Application.py
@@ -9,31 +9,39 @@ from PyQt5.QtCore import Qt
 class MainWindow(QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
-        self.setWindowTitle("Application Title")
+
+        # SIGNAL: The connected function will be called whenever the window
+        # title is changed. The new title will be passed to the function.
+        self.windowTitleChanged.connect(self.onWindowTitleChange)
+
+        # SIGNAL: The connected function will be called whenever the window
+        # title is changed. The new title is discarded in the lambda and the
+        # function is called without parameters.
+        self.windowTitleChanged.connect(lambda x: self.my_custom_fn('tttt',x))
+
+        # SIGNAL: The connected function will be called whenever the window
+        # title is changed. The new title is passed to the function
+        # and replaces the default parameter
+        self.windowTitleChanged.connect(lambda x: self.my_custom_fn(x))
+
+        # SIGNAL: The connected function will be called whenever the window
+        # title is changed. The new title is passed to the function
+        # and replaces the default parameter. Extra data is passed from
+        # within the lambda.
+        self.windowTitleChanged.connect(lambda x: self.my_custom_fn(x, 25))
+
+        self.setWindowTitle("Application Title 114")
+
         label = QLabel("Label text")
         label.setAlignment(Qt.AlignCenter)
-
         self.setCentralWidget(label)
 
-# class Rectangle:
-#     def __init__(self, x_size, y_size):
-#         self.x_size = x_size
-#         self.y_size = y_size
-#
-#     def area(self):
-#         return self.x_size * self.y_size
+    def onWindowTitleChange(self, s):
+        print(s)
 
-
-# class Square(Rectangle):
-#     def __init__(self, x_size):
-#         super().__init__(x_size, x_size)
-#
-#
-# r = Rectangle(3, 5)
-# print(r.area())
-# print('------------------------------')
-# s = Square(6)
-# print(s.area())
+    # SLOT: This has default parameters and can be called without a value
+    def my_custom_fn(self, a="HELLLO!", b=5):
+        print(a, b)
 
 app = QApplication(sys.argv)
 
