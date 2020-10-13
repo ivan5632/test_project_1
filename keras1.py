7fb56fde99d61229e2821a348452ff67c4ca73d7
Ivan Budnikov <ivan34543799@gmail.com>
2020-10-13 18:04:57 +1030

    first model created

--- keras1.py
+++ keras1.py
@@ -6,6 +6,9 @@ from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Activation, Dense
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.metrics import categorical_crossentropy
 
 train_samples = []
 train_labels = []
@@ -40,5 +43,16 @@ train_samples, train_labels = shuffle(train_samples, train_labels)
 scaler = MinMaxScaler(feature_range=(0,1))
 scaled_train_samples = scaler.fit_transform(train_samples.reshape(-1,1))
 
-print(scaled_train_samples)
+# for i, sample in enumerate(zip(scaled_train_samples,train_labels),1):
+#     print(i, ': ', sample)
+print('input shape: ', np.shape(scaled_train_samples))
 
+model = Sequential([
+    Dense(units=16, input_shape=(1,), activation='relu'),
+    Dense(units=32, activation='relu'),
+    Dense(units=2, activation='softmax')
+])
+print(model.summary())
+
+model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.fit(x=scaled_train_samples, y=train_labels, batch_size=10, epochs=30, shuffle=True, verbose=1)
