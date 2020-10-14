f6067a0b3b845cb9b75b34e7c539491c252de8ad
Ivan Budnikov <ivan34543799@gmail.com>
2020-10-14 17:00:51 +1030

    played with medical trial model

--- keras1.py
+++ keras1.py
@@ -12,26 +12,46 @@ from tensorflow.keras.metrics import categorical_crossentropy
 
 train_samples = []
 train_labels = []
+test_samples = []
+test_labels = []
 
 for i in range(50):
     random_younger = randint(13, 64)
     train_samples.append(random_younger)
     train_labels.append(1)
 
-for i in range(50):
     random_older = randint(65, 100)
     train_samples.append(random_older)
     train_labels.append(0)
+
 
 for i in range(1000):
     random_younger = randint(13, 64)
     train_samples.append(random_younger)
     train_labels.append(0)
 
-for i in range(1000):
     random_older = randint(65, 100)
     train_samples.append(random_older)
     train_labels.append(1)
+
+for i in range(10):
+    random_younger = randint(13, 64)
+    test_samples.append(random_younger)
+    test_labels.append(1)
+
+    random_older = randint(65, 100)
+    test_samples.append(random_older)
+    test_labels.append(0)
+
+
+for i in range(200):
+    random_younger = randint(13, 64)
+    test_samples.append(random_younger)
+    test_labels.append(0)
+
+    random_older = randint(65, 100)
+    test_samples.append(random_older)
+    test_labels.append(1)
 
 # for i, side_effect in enumerate(train_labels, 1):
 #     print(i, ': ', side_effect)
@@ -40,8 +60,15 @@ train_samples = np.array(train_samples)
 train_labels = np.array(train_labels)
 train_samples, train_labels = shuffle(train_samples, train_labels)
 
+test_samples = np.array(test_samples)
+test_labels = np.array(test_labels)
+test_samples, test_labels = shuffle(test_samples, test_labels)
+
+
 scaler = MinMaxScaler(feature_range=(0,1))
 scaled_train_samples = scaler.fit_transform(train_samples.reshape(-1,1))
+
+scaled_test_samples = scaler.fit_transform(test_samples.reshape(-1,1))
 
 # for i, sample in enumerate(zip(scaled_train_samples,train_labels),1):
 #     print(i, ': ', sample)
@@ -55,4 +82,68 @@ model = Sequential([
 print(model.summary())
 
 model.compile(optimizer=Adam(learning_rate=0.0001), loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(x=scaled_train_samples, y=train_labels, batch_size=10, epochs=30, shuffle=True, verbose=1)
+model.fit(x=scaled_train_samples, y=train_labels, validation_split=0.1, batch_size=10, epochs=30, verbose=1)
+
+predictions = model.predict(x=scaled_test_samples, batch_size=10, verbose=1)
+for p in predictions:
+    print(p)
+rounded_predictions = np.argmax(predictions, axis=-1)
+for rp in rounded_predictions:
+    print(rp)
+
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import itertools
+
+cm = confusion_matrix(y_true=test_labels, y_pred=rounded_predictions)
+
+def plot_confusion_matrix(cm, classes,
+                        normalize=False,
+                        title='Confusion matrix',
+                        cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, cm[i, j],
+            horizontalalignment="center",
+            color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+cm_plot_labels = ['no_side_effects', 'had_side_effects']
+plot_confusion_matrix(cm=cm, classes=cm_plot_labels)
+
+plt.show()
+
+import os.path
+file_name = 'keras_models/medical_trial_model_20201014.h5'
+if os.path.isfile(file_name) is False:
+    model.save(file_name)
+
+new_model = keras.models.load_model(file_name)
+print(new_model.summary())
+print(new_model.get_weights())
+
+print(model.to_json())
+
